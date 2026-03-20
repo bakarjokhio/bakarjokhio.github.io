6cc9938381cdf77a1591c1922848b8a2b69f8af9
bakarjokhio <61370010+bakarjokhio@users.noreply.github.com>
2026-03-20 08:17:39 +0500

    Add initial app data to data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+const apps = [
+  {
+    name: "Eid Outfit Roast 😂🔥",
+    link: "https://bakarjokhio.github.io/eid-roast/",
+    image: "https://via.placeholder.com/300x200?text=Eid+Roast"
+  },
+  {
+    name: "Eidi Calculator 💸",
+    link: "#",
+    image: ""
+  },
+  {
+    name: "Chand Raat Simulator 🌙",
+    link: "#",
+    image: ""
+  },
+  {
+    name: "More Coming Soon 👀",
+    link: "#",
+    image: ""
+  }
+];
